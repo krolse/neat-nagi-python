c209afbebeac7aab7e0b82adabe2bfebf31f7b2a
krolse <kris1olsen@gmail.com>
2019-09-25 14:52:24 +0200

    Added Genome class and HiddenNodeGene subclass

--- nagi/neat.py
+++ nagi/neat.py
@@ -8,15 +8,53 @@ class LearningRule(Enum):
     symmetric_anti_hebbian = 4
 
 
+class NodeType(Enum):
+    input = 1
+    output = 2
+    hidden = 3
+
+
 class NodeGene(object):
-    def __init__(self, key: int, is_inhibitory: bool, learning_rule: LearningRule):
+    def __init__(self, key: int, node_type: NodeType):
         self.key = key
+        self.node_type = node_type
+
+
+class HiddenNodeGene(NodeGene):
+    def __init__(self, key: int, node_type: NodeType, is_inhibitory: bool, learning_rule: LearningRule):
+        super(HiddenNodeGene, self).__init__(key, node_type)
         self.is_inhibitory = is_inhibitory
         self.learning_rule = learning_rule
 
 
 class ConnectionGene(object):
-    def __init__(self, in_node: int, out_node: int):
+    def __init__(self, in_node: int, out_node: int, innovation_number: int):
         self.in_node = in_node
         self.out_node = out_node
-        self.is_active = True
+        self.innovation_number = innovation_number
+        self.enabled = True
+
+
+class Genome(object):
+    def __init__(self, key: int, input_size: int, output_size: int):
+        self.key = key
+        self.input_size = input_size
+        self.output_size = output_size
+
+        self.nodes = {}
+        self.connections = {}
+        self.input_keys = [i for i in range(input_size)]
+        self.output_keys = [i for i in range(input_size, input_size + output_size)]
+
+        # Initialize node and connection genes for inputs and outputs.
+        # TODO: Innovation numbers need to be kept track of by the overbearing run of the NEAT-algorithm.
+        # TODO: Should self.nodes and self.connections be dictionairies or lists?
+
+        innovation_number = 0
+        for input_key in self.input_keys:
+            self.nodes[input_key] = NodeGene(input_key, NodeType.input)
+            for output_key in self.output_keys:
+                if self.nodes[output_key] is None:
+                    self.nodes[output_key] = NodeGene(output_key, NodeType.output)
+                self.connections[innovation_number] = ConnectionGene(input_key, output_key, innovation_number)
+                innovation_number += 1
