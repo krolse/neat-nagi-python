73f24b97479d959df20163b177848ed98319a726
krolse <kris1olsen@gmail.com>
2019-11-27 19:33:45 +0100

    Added some stuff to visualization test

--- tests/visualization.py
+++ tests/visualization.py
@@ -4,17 +4,22 @@ from itertools import count
 import matplotlib.pyplot as plt
 
 test_genome = Genome(0, 3, 2, count(0), is_initial_genome=True)
-
-nodes = [key for key in test_genome.nodes.keys()]
-edges = [(connection.in_node, connection.out_node) for connection in test_genome.connections.values()]
-
-g = nx.DiGraph()
-g.add_nodes_from(nodes)
-g.add_edges_from(edges)
+# for i in range(10):
+#     test_genome.mutate()
 pos = {0: [0, 2],
        1: [0, 1],
        2: [0, 0],
        3: [1, 0.5],
        4: [1, 1.5]}
-nx.draw(g, with_labels=True, pos=pos, font_weight='bold')
+
+nodes = [key for key in test_genome.nodes.keys()]
+node_color = ['b' if node < 3 else 'r' if node < 5 else 'g' for node in nodes]
+
+edges = [(connection.in_node, connection.out_node) for connection in test_genome.connections.values()]
+
+g = nx.DiGraph()
+g.add_nodes_from(nodes)
+g.add_edges_from(edges)
+
+nx.draw_networkx(g, pos=pos, with_labels=True, nodes=nodes, node_color=node_color, font_color="w")
 plt.show()
