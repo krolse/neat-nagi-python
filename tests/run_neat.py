27453dc116050e4335f0a67b945d6ab0cfa62721
krolse <kris1olsen@gmail.com>
2020-03-30 20:36:01 +0200

    change to how files are named

--- tests/run_neat.py
+++ tests/run_neat.py
@@ -8,12 +8,21 @@ import tqdm
 from nagi.neat import Population
 from nagi.simulation_1d import OneDimensionalEnvironment, OneDimensionalAgent
 
+
+def get_file_path():
+    root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    run_number = 1
+    while os.path.exists(root_path + f'/data/test_run_{run_number}.pkl'):
+        run_number += 1
+    return root_path + f'/data/test_run_{run_number}.pkl'
+
+
 if __name__ == '__main__':
+    path = get_file_path()
     pool = mp.Pool(mp.cpu_count())
 
-    population = Population(100, 4, 2)
+    population = Population(2, 4, 2)
     generations = {}
-
     for i in range(0, 100):
         print(f'\nGeneration {i}...')
         env = OneDimensionalEnvironment(50, 5)
@@ -22,7 +31,7 @@ if __name__ == '__main__':
         fitnesses = {result[0]: result[1] for result in results}
         generations[i] = {'population': deepcopy(population), 'fitnesses': fitnesses}
 
-        with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/data/test_run_7.pkl', 'wb') as file:
+        with open(path, 'wb') as file:
             pickle.dump(generations, file)
 
         population.next_generation(fitnesses)
--- tests/visualize_test_run.py
+++ tests/visualize_test_run.py
@@ -16,12 +16,10 @@ def get_most_fit_genome(results: Dict[int, Dict]):
 
 
 if __name__ == '__main__':
-    with open('../data/test_run_5.pkl', 'rb') as file:
+    with open('../data/test_run_7.pkl', 'rb') as file:
         data = pickle.load(file)
 
     genome = get_most_fit_genome(data)
-    with open('../data/top_genome_test_run_5.pkl', 'wb') as file:
-        pickle.dump(genome, file)
     visualize_genome(genome)
     # max_fitnesses.append(max(fitnesses.values()))
     # average_fitnesses.append(sum(fitnesses.values()) / len(fitnesses))
