a17a3218f84bfa425ef973304ff4a6d0e6234a35
krolse <kris1olsen@gmail.com>
2020-04-02 14:52:52 +0200

    fetched genome from test_run_1 (large)

--- tests/simulation_1d.py
+++ tests/simulation_1d.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 
 import pickle
 
-with open('../data/most_fit_genome_test_run_1.pkl', 'rb') as file:
+with open('../data/top_genome_test_run_1.pkl', 'rb') as file:
     test_genome = pickle.load(file)
 
 agent = OneDimensionalAgent.create_agent(test_genome)
 visualize_genome(test_genome, True)
-environment = OneDimensionalEnvironment(100, 5)
+environment = OneDimensionalEnvironment(50, 5)
 _, fitness, weights, membrane_potentials, time_step = environment.simulate_with_visualization(agent)
 
 t_values = range(time_step + 1)
