a648adbde2fdd231c8c52bb87629ce14eecb3a09
krolse <kris1olsen@gmail.com>
2020-04-10 13:36:17 +0200

    Added plot of actuators over time

--- tests/simulation_1d.py
+++ tests/simulation_1d.py
@@ -11,11 +11,31 @@ with open('../data/most_fit_genome_test_run_5.pkl', 'rb') as file:
 agent = OneDimensionalAgent.create_agent(test_genome)
 visualize_genome(test_genome, True)
 environment = OneDimensionalEnvironment(50, 5)
-_, fitness, weights, membrane_potentials, time_step, intervals = environment.simulate_with_visualization(agent)
+_, fitness, weights, membrane_potentials, time_step, intervals, actuators = environment.simulate_with_visualization(
+    agent)
 
-t_values = range(time_step + 1)
 number_of_neurons = len(membrane_potentials.keys())
 number_of_weights = len(weights.keys())
+t_values = range(time_step + 1)
+
+
+def add_vertical_lines_and_background(height: int):
+    flip_points = [flip_point for flip_point in range(len(t_values))
+                   if flip_point >= FLIP_POINT_1D * NUM_TIME_STEPS
+                   and flip_point % (FLIP_POINT_1D * NUM_TIME_STEPS) == 0]
+    sample_points = [sample_point for sample_point in range(len(t_values)) if
+                     sample_point >= NUM_TIME_STEPS and
+                     sample_point % NUM_TIME_STEPS == 0 and
+                     sample_point not in flip_points]
+
+    for flip_point in flip_points:
+        plt.axvline(x=flip_point, color='k')
+    for sample_point in sample_points:
+        plt.axvline(x=sample_point, color='gray', linestyle='--')
+    for start, end in intervals:
+        rect = plt.Rectangle((start, 0), end - start, height=height, facecolor=RED)
+        plt.gca().add_patch(rect)
+
 
 # Membrane potential
 fig = plt.figure()
@@ -28,22 +48,7 @@ for i, key in enumerate(sorted(membrane_potentials.keys())):
              color=GREEN, linestyle='-')
     plt.plot(t_values, [membrane_potential[1] for membrane_potential in membrane_potentials[key]],
              color=BLUE, linestyle='-')
-    flip_points = [i for i in range(len(membrane_potentials[key])) if
-                   i >= FLIP_POINT_1D * NUM_TIME_STEPS and i % (FLIP_POINT_1D * NUM_TIME_STEPS) == 0]
-    sample_points = [i for i in range(len(membrane_potentials[key])) if
-                     i >= NUM_TIME_STEPS and
-                     i % NUM_TIME_STEPS == 0 and
-                     i not in flip_points]
-
-    for flip_point in flip_points:
-        plt.axvline(x=flip_point, color='k')
-    for sample_point in sample_points:
-        plt.axvline(x=sample_point, color='gray', linestyle='--')
-    for start, end in intervals:
-        height = 4
-        rect = plt.Rectangle((start, 0), end - start, height=height, facecolor=RED)
-        plt.gca().add_patch(rect)
-
+    add_vertical_lines_and_background(4)
 
 # Weights
 fig = plt.figure()
@@ -53,21 +58,15 @@ for i, key in enumerate(sorted(weights.keys(), key=lambda x: x[1])):
     plt.ylabel(f"{key}")
     plt.xlabel("Time (in ms)")
     plt.plot(t_values, weights[key], color=BLUE, linestyle='-')
-    flip_points = [i for i in range(len(weights[key])) if
-                   i >= FLIP_POINT_1D * NUM_TIME_STEPS and i % (FLIP_POINT_1D * NUM_TIME_STEPS) == 0]
-    sample_points = [i for i in range(len(weights[key])) if
-                     i >= NUM_TIME_STEPS and
-                     i % NUM_TIME_STEPS == 0 and
-                     i not in flip_points]
+    add_vertical_lines_and_background(2)
 
-    for flip_point in flip_points:
-        plt.axvline(x=flip_point, color='k')
-    for sample_point in sample_points:
-        plt.axvline(x=sample_point, color='gray', linestyle='--')
-    for start, end in intervals:
-        height = 2
-        rect = plt.Rectangle((start, 0), end - start, height=height, facecolor="red", alpha=0.5)
-        plt.gca().add_patch(rect)
-
+# Actuator history
+fig = plt.figure()
+plt.title("Actuator history")
+eat_actuators = [actuator[0] for actuator in actuators]
+avoid_actuators = [actuator[1] for actuator in actuators]
+plt.plot(t_values, eat_actuators, color=GREEN)
+plt.plot(t_values, avoid_actuators, color=BLUE)
+add_vertical_lines_and_background(max(max(eat_actuators), max(avoid_actuators)) + 2)
 print(f'Fitness: {fitness}')
 plt.show()
